cb2f836d83cc44061356d3ba781f99d348df9a1d
ldz <ldz@ask.com>
2019-12-09 16:08:13 +0800

    1

---
+++ vue/src/l/arithmetic.java
@@ -0,0 +1,19 @@
+package l;
+
+public class arithmetic {
+    public static void main(String[] args) {
+        String a = "qwewqe";
+        String b = a.replace("q", "");
+        a.indexOf("w");
+        System.out.printf(b);
+    }
+    public int numJewelsInStones(String J, String S) {
+        char[]j = J.toCharArray();
+        int a = J.length();
+        for (int k=0; k<j.length; k++) {
+            J.replace(String.valueOf(j[k]),"");
+        }
+
+        return a-J.length();
+    }
+}
