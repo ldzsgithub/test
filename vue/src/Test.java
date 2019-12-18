f94fc6086b4d37e87005d91bb896672571c03da7
ldz <ldz@ask.com>
2019-12-18 09:18:30 +0800

    1

--- vue/src/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        String a= new String("12212");
-        System.out.println(a);
-
-        StringBuilder b =new StringBuilder("12123");
-    }
-}
